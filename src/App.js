3d90c98d4960da239d6fbebded9ad33972026111
GousosGas <msf7415003@teilar.gr>
2020-08-08 04:16:29 +0300

    Style fix and loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar/AppBar';
-import Container from '@material-ui/core/Container';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import BookPage from './pages/BookPage/BookPage';
 import HomePage from './pages/HomePage/HomePage';
 import styles from './App.module.scss';
-import NavTitle from './core/NavTitle/NavTitle';
+import NavTitle from './core/components/NavTitle/NavTitle';
 import Footer from './components/Footer/Footer';
 import BookForm from './pages/BookForm/BookForm';
+import Loader from './core/components/Loader/Loader';
+import { uiSelector } from './core/selectors/bookResultSelectors';
 
 /** *
  * The main app component for Bookstore App
@@ -19,19 +22,26 @@ import BookForm from './pages/BookForm/BookForm';
  * @see BookPage
  * @see BookForm
  */
-const App = () => (
+const App = ({ loading }) => (
   // eslint-disable-next-line react/jsx-filename-extension
   <div>
+    {loading ? <Loader /> : null}
     <AppBar position="static" color="transparent" class={styles.BackGroundColor}>
-      <NavTitle tag="h3" title="BookStore" align="left" />
+      <NavTitle tag="h3" title="myBookStore" align="center" />
     </AppBar>
     <Route exact path="/" component={HomePage} />
     <Route exact path="/create" component={BookForm} />
     <Route exact path="/book/:id" component={BookPage} />
-    <Container maxWidth="md">
-      <Footer />
-    </Container>
+    <Footer />
   </div>
 );
 
-export default App;
+App.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
+
+export const mapStateToProps = (state) => ({
+  loading: uiSelector(state),
+});
+
+export default connect(mapStateToProps)(App);
